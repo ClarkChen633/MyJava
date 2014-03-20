021b7dad7b3ef30c44a0872aadee866057f878db
ClarkChen <clarkchen633@gmail.com>
2014-03-20 16:06:22 +0800

    It add the Hello Java example.

---
+++ Hello/src/Hello.java
@@ -0,0 +1,26 @@
+
+public class Hello {
+	
+	void method() 
+	{
+		int i = 0;
+		while(i < 10)
+		{
+			i++;
+			System.out.println("Value = " +  i);
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Hello test = new Hello();
+        test.method();
+	}
+
+
+	    
+
+}
